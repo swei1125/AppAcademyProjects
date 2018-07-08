33666002bda943fd6cb593daaf6cce0f56e12165
Natasha <wsyalways2@gmail.com>
2018-07-08 01:10:30 -0700

    update w3d4 polls app

--- W3D4-Movie_buff,Polls/polls_app/app/models/question.rb
+++ W3D4-Movie_buff,Polls/polls_app/app/models/question.rb
@@ -11,17 +11,32 @@
 
 class Question < ApplicationRecord
   validates :body, :poll_id, presence: true
+
   has_many :answer_choices,
-    primary_key: :id, 
+    primary_key: :id,
     foreign_key: :question_id,
     class_name: :AnswerChoice
-    
+
   belongs_to :poll,
     primary_key: :id,
     foreign_key: :poll_id,
     class_name: :Poll
-    
+
   has_many :responses,
     through: :answer_choices,
     source: :reponses
-end 
+
+  def results
+    choices_count = self
+      .answer_choices
+      .select('answer_choices.*, count(responses.id) as reponses_count')
+      .left_outer_joins(:responses)
+      .group('answer_choices.id')
+
+    result = {}
+    choices_count.each do |choice|
+      result[choice.body] = choice.responses_count
+    end
+    result
+  end
+end
