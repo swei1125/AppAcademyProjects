03eb78acc609a1623bc86e24aa9390584ef6337b
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-09 17:33:41 -0700

    complete first routes and add first controller

---
+++ first_routes/app/models/artwork.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string           not null
+#  image_url  :string           not null
+#  artist_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Artwork < ApplicationRecord
+  validates :artist_id, :title, :image_url, presence: true 
+
+  belongs_to :artist,
+    foreign_key: :artist_id,
+    class_name: :User 
+    
+  has_many :shared_artworks,
+  foreign_key: :artwork_id,
+  class_name: :ArtworkShare
+  
+  has_many :shared_viewers,
+  through: :shared_artworks,
+  source: :viewer
+end 
