bdd4aaed6da419a7f1a79523a3a5557ef6acbaf8
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-11 18:04:32 -0700

    add w4d3 project

---
+++ W4D3-cats_auth/app/controllers/cat_rental_requests_controller.rb
@@ -0,0 +1,53 @@
+class CatRentalRequestsController < ApplicationController
+  before_action :must_be_owner, only: [:approve, :deny]
+  
+  def approve
+    current_cat_rental_request.approve!
+    redirect_to cat_url(current_cat)
+  end
+
+  def create
+    @rental_request = CatRentalRequest.new(cat_rental_request_params)
+    @rental_request.user_id = current_user.id
+    if @rental_request.save
+      redirect_to cat_url(@rental_request.cat)
+    else
+      flash.now[:errors] = @rental_request.errors.full_messages
+      render :new
+    end
+  end
+
+  def deny
+    current_cat_rental_request.deny!
+    redirect_to cat_url(current_cat)
+  end
+
+  def new
+    @rental_request = CatRentalRequest.new
+    
+  end
+
+  private
+
+  def current_cat_rental_request
+    @rental_request ||=
+      CatRentalRequest.includes(:cat).find(params[:id])
+  end
+
+  def current_cat
+    current_cat_rental_request.cat
+  end
+
+  def cat_rental_request_params
+    params.require(:cat_rental_request).permit(:cat_id, :end_date, :start_date, :status)
+  end
+  
+  def is_owner?
+    current_user == current_cat.owner 
+  end
+  
+  def must_be_owner
+    request = CatRentalRequest.find(params[:id])
+    redirect_to cat_rental_url(request) unless is_owner?
+  end
+end
